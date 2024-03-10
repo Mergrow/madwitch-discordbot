1cd5cbd4eac838ba700c3eb2cf8cf1cabd9a0b66
Mergrow <mergrow@protonmail.com>
2024-03-10 06:18:36 -0300

    custom rpc

--- bot/cogs/rpc.py
+++ bot/cogs/rpc.py
@@ -11,6 +11,21 @@ class _rpc(commands.Cog):
         self.witch = witch
 
     @commands.command()
+    async def rpc(self, ctx, type, rpc):
+        if ctx.author.id == ownerid:
+            if type == "streaming":
+                await ctx.send(f"Status RPC atualizado para **{type}** {rpc}", delete_after=1)
+                print("Status RPC atualizado para Transmitindo.")
+                await client.change_presence(activity=discord.Streaming(name=f'{rpc}', url='https://www.twitch.tv/mergrow_', status=discord.Status.idle))
+            elif type == "playing":
+                await client.change_presence(activity=discord.Game(name=f'{rpc}', status=discord.Status.idle))
+                await ctx.send(f"Status RPC atualizado para **{type}** {rpc}", delete_after=1)
+                print("Status RPC atualizado para Jogando.")
+            elif type != "playing" and type != "streaming":
+                await ctx.send("Tipo não reconhecido. Use `live` ou `playing`.")    
+        else: return await ctx.send("Você não tem permissão para executar este comando!")
+                
+    @commands.command()
     async def rpc1(self, ctx):
         if ctx.author.id == ownerid:
             await ctx.send('Status RPC atualizado para **Transmitindo** com sucesso!',  delete_after=1)
